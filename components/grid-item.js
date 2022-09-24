6c5064402ebe832cf7a4eed1eb7907e6378bff5a
Hatem Lamine <hatelm902@gmail.com>
2022-09-24 20:43:00 +0100

    feature : adjusted footer spacing in /Projects

--- components/grid-item.js
+++ components/grid-item.js
@@ -62,7 +62,7 @@ export const WorkGridItem = ({
   const [show, setShow] = useState(false)
   const handleToggle = () => setShow(!show)
   return (
-    <Box w="100%" textAlign="center">
+    <Box w="100%" textAlign="center" pb={showMoreHide && 4}>
       <LinkBox
         cursor="pointer "
         transition="ease"
